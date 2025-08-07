18ed9d3c64e75cfef354e736fc9037ec31a88164
DESKTOP-RRI0NFM\pashi <pashiran@gmail.com>
2025-08-07 23:55:05 +0900

    F코드 뮤직박스와 C 코드 뮤직박스에 대응하게 코드 수정(테스트는 하지 못함)

--- main.py
+++ main.py
@@ -21,7 +21,10 @@ paper_max_width = 13.7*25.4 #max horizontal width of paper being cut
 paper_max_height = 10.9*25.4 #max vertical height of paper being cut
 
 #list of notes currently available on music box
-note_names_available = ['F3','G3','C4','D4','E4','F4','G4','A4','A#4','B4','C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','C#6','D6','D#6','E6','F6','G6','A6']
+#F_note_names_available = ['F3','G3','C4','D4','E4','F4','G4','A4','A#4','B4','C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','C#6','D6','D#6','E6','F6','G6','A6']
+C_note_names_available = ['C3','D3','G3','A3','B3','C4','D4','E4','F4','F#4','G4','G#4','A4','A#4','B4','C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','D6','E6']
+#note_names_available = F_note_names_available
+note_names_available = C_note_names_available
 
 # -----------------------------------------END OF GLOBALS----------------------------------------------------------
 
